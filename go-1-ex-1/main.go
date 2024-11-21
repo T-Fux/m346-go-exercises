8aa1e66221730eaa3db12239de63ed598890994e
Timo Fuchs <Timo_Fuchs@sluz.ch>
2024-11-21 16:12:45 +0100

    Task1

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -1,8 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
+	var firstName string = "Timo"
+	lastName := "Fuchs"
+
+	var dayOfBirth int = 14
+	monthOfBirth := 9
+	yearOfBirth := 2006
+
+	var numberOfSiblings int = 3
+	var heightInMeters float32 = 1.73
+	var zodiacSign rune = 'v'
+
 	// TODO: Declare and initialize the variables being used in the output!
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
