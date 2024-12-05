4111cf24da9a5573b2922d3cc4d122a080586723
Timo Fuchs <Timo_Fuchs@sluz.ch>
2024-12-05 15:55:45 +0100

    Kapitel 2, Task 1

--- go-2-ex-1/main.go
+++ go-2-ex-1/main.go
@@ -4,12 +4,22 @@ import "fmt"
 
 type FullName struct {
 	// TODO: add fields
+	FirstName string
+	LastName  string
 }
 
 // TODO: declare a structure for birth date
 
+type BirthDate struct {
+	dayOfBirth   int
+	monthOfBirth int
+	yearOfBirth  int
+}
+
 type Profile struct {
 	// TODO: embed full name and birth date information
+	Name             FullName
+	Birth            BirthDate
 	NumberOfSiblings byte
 	ZodiacSign       rune
 }
@@ -17,12 +27,22 @@ type Profile struct {
 func main() {
 	var me = Profile{
 		// TODO: set name and birth date information
-		NumberOfSiblings: 0,   // TODO: adjust
-		ZodiacSign:       ' ', // TODO: adjust
+		Name: FullName{
+			FirstName: "Timo",
+			LastName:  "Fuchs",
+		},
+		Birth: BirthDate{
+			dayOfBirth:   14,
+			monthOfBirth: 9,
+			yearOfBirth:  2006,
+		},
+		NumberOfSiblings: 3,        // TODO: adjust
+		ZodiacSign:       '\u264d', // TODO: adjust
 	}
 	fmt.Println(me)
 
 	fmt.Println("Siblings Before:", me.NumberOfSiblings)
 	// TODO: imagine, you get a little brother or sister
+	me.NumberOfSiblings++
 	fmt.Println("Siblings After:", me.NumberOfSiblings)
 }
